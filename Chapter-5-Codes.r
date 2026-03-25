b98befec33e0d5725060f47454d805f90815cfca
Vaibhav Rajendra Adsul <vaibhavadsul5564@gmail.com>
2026-03-25 04:47:09 +0000

    Chapter 5 Completed

---
+++ Chapter-5-Codes.r
@@ -0,0 +1,25 @@
+x <- c(1, 2, 3, 4, 5)
+y <- c(2, 4, 6, 8, 10)
+
+plot(x, y, main = "Scatter Plot", xlab = "X", ylab = "Y")
+
+cor(x, y, method = "pearson")
+cor(x, y, method = "spearman")
+
+# Regression model
+model <- lm(y ~ x)
+
+# Summary
+summary(model)
+
+# Prediction
+predict(model, data.frame(x=7))
+
+# R squared
+''' 
+R² = 0 → bad model
+R² = 1 → perfect model 
+'''
+summary(model)$r.squared
+
+plot(model)
