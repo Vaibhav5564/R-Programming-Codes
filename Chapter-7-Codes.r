c095ae49a8f6b33c13cb7894a54bb10847f1093c
Vaibhav Rajendra Adsul <vaibhavadsul5564@gmail.com>
2026-03-25 06:36:42 +0000

    Chapter 7 Completed

---
+++ Chapter-7-Codes.r
@@ -0,0 +1,37 @@
+# t-test	                        Z-test
+# Small sample	                  Large sample
+# SD unknown	                   SD known
+# Uses t-distribution	     Uses normal distribution
+
+# Formula for Z test
+
+# xbar <- 52
+# mu <- 50
+# sigma <- 10
+# n <- 100
+
+# z <- (xbar - mu) / (sigma / sqrt(n))
+# z
+
+
+# One Sample t-test
+# x <- c(45, 50, 55, 60, 52)
+# t.test(x, mu=50)
+
+# Two Sample t-test
+# y <- c(40, 42, 48, 50, 47)
+# t.test(x, y)
+
+# paired Test
+# t.test(x, y, paired = TRUE)
+
+
+# Z test manual
+
+# xbar = 52
+# mu = 50
+# sigma = 10
+# n = 100
+
+# z = (xbar - mu)/(sigma / sqrt(n))
+# z
