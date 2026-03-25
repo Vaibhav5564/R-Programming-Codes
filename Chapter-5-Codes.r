5e3b714c42b943c3448c681273062333d83b1fdc
Vaibhav Rajendra Adsul <vaibhavadsul5564@gmail.com>
2026-03-25 06:41:07 +0000

    Chapter 5 Completed

--- Chapter-5-Codes.r
+++ Chapter-5-Codes.r
@@ -1,25 +1,25 @@
-x <- c(1, 2, 3, 4, 5)
-y <- c(2, 4, 6, 8, 10)
+# x <- c(1, 2, 3, 4, 5)
+# y <- c(2, 4, 6, 8, 10)
 
-plot(x, y, main = "Scatter Plot", xlab = "X", ylab = "Y")
+# plot(x, y, main = "Scatter Plot", xlab = "X", ylab = "Y")
 
-cor(x, y, method = "pearson")
-cor(x, y, method = "spearman")
+# cor(x, y, method = "pearson")
+# cor(x, y, method = "spearman")
 
 # Regression model
-model <- lm(y ~ x)
+# model <- lm(y ~ x)
 
 # Summary
-summary(model)
+# summary(model)
 
 # Prediction
-predict(model, data.frame(x=7))
+# predict(model, data.frame(x=7))
 
 # R squared
-''' 
-R² = 0 → bad model
-R² = 1 → perfect model 
-'''
-summary(model)$r.squared
 
-plot(model)
+# R² = 0 → bad model
+# R² = 1 → perfect model 
+
+# summary(model)$r.squared
+
+# plot(model)
